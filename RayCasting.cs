a8b9594ab0dc15539ce6a2e08468c0de54db42ad
Rúni Gudmundarson <78358597+Raygud@users.noreply.github.com>
2023-02-22 16:52:39 +0100

    Update RayCasting.cs

--- RayCasting.cs
+++ RayCasting.cs
@@ -1,42 +1,47 @@
-public void Main(string argument, UpdateType updateType)
-{
-    IMyTerminalBlock block = GridTerminalSystem.GetBlockWithName("MyCamera");
-    if (block != null && block is IMyCameraBlock)
-    {
-        IMyCameraBlock camera = block as IMyCameraBlock;
+IMyCameraBlock camera;
+        IMyTextPanel lcd;
+        public Program()
+		{
 
-        block = GridTerminalSystem.GetBlockWithName("MyLCDPanel");
-        if (block != null && block is IMyTextPanel)
+            camera = GridTerminalSystem.GetBlockWithName("Camera") as IMyCameraBlock;
+            if (camera == null)
+            {
+                throw new Exception("Couldn't find camera!");
+
+            }
+
+            camera.EnableRaycast = true;
+
+            lcd = GridTerminalSystem.GetBlockWithName("LCD") as IMyTextPanel;
+            if (lcd == null)
+            {
+                throw new Exception("Couldn't find LCD!");
+
+            }
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+		}
+
+        public void Main(string argument, UpdateType updateType)
         {
-            IMyTextPanel textPanel = block as IMyTextPanel;
+          
 
             double distance = 200; // distance to raycast
             MyDetectedEntityInfo entity = camera.Raycast(distance);
-
-            if (entity.Type != MyDetectedEntityType.None && entity.HitPosition.HasValue)
+            if (entity.Type != MyDetectedEntityType.None)
             {
                 Vector3D hitPosition = entity.HitPosition.Value;
                 Vector3D cameraPosition = camera.GetPosition();
-                double groundDistance = hitPosition.Y - cameraPosition.Y;
+                double groundDistance = Vector3D.Distance(hitPosition, cameraPosition);
 
-                // Display information on LCD screen
-                textPanel.WriteText("Ground Distance: " + groundDistance.ToString("#.##") + " m\n", false);
-                textPanel.WriteText("Entity Name: " + entity.Name + "\n", true);
-                textPanel.WriteText("Entity Type: " + entity.Type.ToString() + "\n", true);
+                // Use groundDistance for whatever you need
+
+                // Refresh LCD with live data
+                lcd.WriteText($"Ground Distance: {groundDistance}m");
             }
             else
             {
-                // Handle case when entity.HitPosition does not have a value
+                // Clear LCD if no entity detected
+                lcd.WriteText("");
             }
-            Runtime.UpdateFrequency = UpdateFrequency.Update10;
         }
-        else
-        {
-            Echo("No display found, please check that you have a LCD screen installed and that it has the right name");
-        }
-    }
-    else
-    {
-        Echo("No camera found, cannot detect distance");
-    }
-}
